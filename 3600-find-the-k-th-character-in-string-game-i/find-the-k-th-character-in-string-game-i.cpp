6a97d299aa851076f4e4da6a5666d8871db75286
Anmol-12345 <anmolgupt871@gmail.com>
2025-07-03 18:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.62%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        //here we mention here the string is "a".
-        string s = "a";
+        // //here we mention here the string is "a".
+        // string s = "a";
 
-        while(s.size()<k)
-        {
-            string t = "";
-            for(int i =0;i<s.size();i++)
-            {
-                //ek extra character string mai add karna hai.
-                t += s[i] + 1;
-            }
-            //aur jo final string t aai hai usse s mai add kar do.
-            s += t;
-        }
-        //matlab isse k se ek pehle wala character chahiye.
-        return s[k-1];
+        // while(s.size()<k)
+        // {
+        //     string t = "";
+        //     for(int i =0;i<s.size();i++)
+        //     {
+        //         //ek extra character string mai add karna hai.
+        //         t += s[i] + 1;
+        //     }
+        //     //aur jo final string t aai hai usse s mai add kar do.
+        //     s += t;
+        // }
+        // //matlab isse k se ek pehle wala character chahiye.
+        // return s[k-1];
+
+        //Other Solution 
+        //this is built in function already in c++.
+        return 'a'+__builtin_popcount(k-1);//it means to count no of 1s in binary representation of k-1;
     }
 };
